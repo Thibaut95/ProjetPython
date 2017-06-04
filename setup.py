d6014b17aca1aeda59a4da042a56af4753522d33
Thibaut <thibaut.piquerez@he-arc.ch>
2017-06-04 15:29:18 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,33 @@
-from Blackjack.connectionbot import *
+from os import path
+from setuptools import setup, find_packages
 
-# Lancer le programme.
-loop = asyncio.get_event_loop()
-loop.set_debug(True)
-loop.run_until_complete(main())
-loop.close()
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Blackjack',
+    python_requires='>=3.6',
+    author='Thibaut Piquerez et Nicolas Kaser',
+    author_email='thibaut.piquerez@he-arc.ch',
+    url='https://github.com/Thibaut95/ProjetPython.git',
+    license='https://opensource.org/licenses/BSD-3-Clause',
+    packages=find_packages(exclude=('contrib', 'docs', 'tests')),
+    classifiers=(
+        'Development Status :: 1 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: entertainment'
+    ),
+    install_requires=(
+        'aiohttp>=2.1.0',
+    )
+)
