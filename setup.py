33d6b068e5f522c95010da735e6eda8903e37669
Thibaut <thibaut.piquerez@he-arc.ch>
2017-06-04 15:52:47 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from Blackjack.connectionbot import *
+from setuptools import setup, find_packages
 
-# Lancer le programme.
-loop = asyncio.get_event_loop()
-loop.set_debug(True)
-loop.run_until_complete(main())
-loop.close()
+setup(
+    name='Blackjack',
+    version='0.1.0',
+    description='Bot Discord',
+    author='Thibaut Piquerez et Nicolas Kaser',
+    author_email='thibaut.piquerez@he-arc.ch',
+    url='https://github.com/Thibaut95/ProjetPython.git',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
